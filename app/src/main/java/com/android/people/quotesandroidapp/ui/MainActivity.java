e88590232b85961ac7a60f5f98957925d2aea0ea
AmrElmasry <amrelmasry.me@gmail.com>
2015-12-13 22:42:09 +0200

    package ui added

--- app/src/main/java/com/android/people/quotesandroidapp/ui/MainActivity.java
+++ app/src/main/java/com/android/people/quotesandroidapp/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.android.people.quotesandroidapp;
+package com.android.people.quotesandroidapp.ui;
 
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.android.people.quotesandroidapp.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
