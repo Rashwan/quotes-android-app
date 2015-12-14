3ebc6ace93bb216fa24ec00d5cdc22d479a0959e
AmrElmasry <amrelmasry.me@gmail.com>
2015-12-14 16:46:29 +0200

    Home quotes randomly changing now.

--- app/src/main/java/com/android/people/quotesandroidapp/utils/DatabaseUtils.java
+++ app/src/main/java/com/android/people/quotesandroidapp/utils/DatabaseUtils.java
@@ -1,6 +1,7 @@
 package com.android.people.quotesandroidapp.utils;
 
 import android.content.Context;
+import android.widget.Toast;
 
 import com.android.people.quotesandroidapp.models.Quote;
 import com.android.people.quotesandroidapp.provider.categories.CategoriesCursor;
@@ -8,15 +9,25 @@ import com.android.people.quotesandroidapp.provider.categories.CategoriesSelecti
 import com.android.people.quotesandroidapp.provider.quotes.QuotesCursor;
 import com.android.people.quotesandroidapp.provider.quotes.QuotesSelection;
 
+import java.util.Random;
+
 /**
  * Created by amrelmasry on 12/14/15.
  */
 public class DatabaseUtils {
     public static Quote getRandomQuote(Context context) {
 
-        // TODO generate random quote
+        // TODO generate random quote in a more efficient way
+
+
         QuotesSelection where = new QuotesSelection();
-        where.id(2);
+        int max = where.count(context.getContentResolver());
+        Toast.makeText(context, "" + max, Toast.LENGTH_SHORT).show();
+
+        Random rand = new Random();
+        int id = rand.nextInt(max);
+
+        where.id(id);
         QuotesCursor quoteCursor = where.query(context);
 
         if (quoteCursor.moveToNext()) {
