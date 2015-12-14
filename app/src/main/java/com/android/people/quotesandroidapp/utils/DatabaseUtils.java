2c44a0a6321c436544542a7789b5a453c7ccafa3
AmrElmasry <amrelmasry.me@gmail.com>
2015-12-14 16:00:24 +0200

    Home quotes categories changing dynamically now

--- app/src/main/java/com/android/people/quotesandroidapp/utils/DatabaseUtils.java
+++ app/src/main/java/com/android/people/quotesandroidapp/utils/DatabaseUtils.java
@@ -3,6 +3,8 @@ package com.android.people.quotesandroidapp.utils;
 import android.content.Context;
 
 import com.android.people.quotesandroidapp.models.Quote;
+import com.android.people.quotesandroidapp.provider.categories.CategoriesCursor;
+import com.android.people.quotesandroidapp.provider.categories.CategoriesSelection;
 import com.android.people.quotesandroidapp.provider.quotes.QuotesCursor;
 import com.android.people.quotesandroidapp.provider.quotes.QuotesSelection;
 
@@ -17,12 +19,26 @@ public class DatabaseUtils {
         where.id(2);
         QuotesCursor quoteCursor = where.query(context);
 
-        if (quoteCursor.getCount() > 0 && quoteCursor.moveToNext()) {
+        if (quoteCursor.moveToNext()) {
 
             return new Quote(quoteCursor.getId(), quoteCursor.getContent(), quoteCursor.getCategoryid());
 
         }
         return null;
 
+    }
+
+    public static String getCategoryName(long categoryID, Context context) {
+
+        CategoriesSelection where = new CategoriesSelection();
+        where.id(categoryID);
+        CategoriesCursor categoryCursor = where.query(context);
+
+        if (categoryCursor.moveToNext()) {
+
+            return categoryCursor.getCategory();
+
+        }
+        return null;
     }
 }
