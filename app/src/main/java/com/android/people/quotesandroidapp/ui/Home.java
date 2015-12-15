adbc73f15971f9605e2a83d36ca6d025d30b7564
AmrElmasry <amrelmasry.me@gmail.com>
2015-12-15 16:10:47 +0200

    Home fragment code clean up

--- app/src/main/java/com/android/people/quotesandroidapp/ui/Home.java
+++ app/src/main/java/com/android/people/quotesandroidapp/ui/Home.java
@@ -14,69 +14,30 @@ import com.android.people.quotesandroidapp.R;
 import com.android.people.quotesandroidapp.models.Quote;
 import com.android.people.quotesandroidapp.utils.DatabaseUtils;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link com.android.people.quotesandroidapp.ui.Home.OnQuoteClickedListener} interface
- * to handle interaction events.
- * Use the {@link Home#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Home extends Fragment implements View.OnClickListener {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnQuoteClickedListener mListener;
 
     public Home() {
-        // Required empty public constructor
     }
 
-    private Quote firstQuote;
-    private Quote secondQuote;
-    private Quote favoriteQuote;
+    // quotes for the two categories cards
+    private Quote mFirstQuote;
+    private Quote mSecondQuote;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Home.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Home newInstance(String param1, String param2) {
-        Home fragment = new Home();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    // quote for favorite card
+    private Quote mFavoriteQuote;
 
-    // only for testing
+
+    // Fragment factory method
     public static Home newInstance() {
-        Home fragment = new Home();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, "gfh");
-        args.putString(ARG_PARAM2, "fdf");
-        fragment.setArguments(args);
-        return fragment;
+        return new Home();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -97,25 +58,25 @@ public class Home extends Fragment implements View.OnClickListener {
         TextView firstQuoteCategory = (TextView) firstQuoteCard.findViewById(R.id.first_quote_title);
         TextView secondQuoteCategory = (TextView) secondQuoteCard.findViewById(R.id.second_quote_title);
 
-        firstQuote = DatabaseUtils.getRandomQuote(getActivity().getApplicationContext());
-        secondQuote = DatabaseUtils.getRandomQuote(getActivity().getApplicationContext());
-        favoriteQuote = DatabaseUtils.getRandomFavoriteQuote(getActivity().getApplicationContext());
+        mFirstQuote = DatabaseUtils.getRandomQuote(getActivity().getApplicationContext());
+        mSecondQuote = DatabaseUtils.getRandomQuote(getActivity().getApplicationContext());
+        mFavoriteQuote = DatabaseUtils.getRandomFavoriteQuote(getActivity().getApplicationContext());
 
         Button first_fav = (Button) firstQuoteCard.findViewById(R.id.first_fav);
         Button second_fav = (Button) secondQuoteCard.findViewById(R.id.second_fav);
 
 
-        if (firstQuote != null) {
-            firstQuoteContent.setText(firstQuote.getContent());
-            firstQuoteCategory.setText(DatabaseUtils.getCategoryName(firstQuote.getCategoryID(), getActivity().getApplicationContext()));
+        if (mFirstQuote != null) {
+            firstQuoteContent.setText(mFirstQuote.getContent());
+            firstQuoteCategory.setText(DatabaseUtils.getCategoryName(mFirstQuote.getCategoryID(), getActivity().getApplicationContext()));
         }
-        if (secondQuote != null) {
-            secondQuoteContent.setText(secondQuote.getContent());
-            secondQuoteCategory.setText(DatabaseUtils.getCategoryName(secondQuote.getCategoryID(), getActivity().getApplicationContext()));
+        if (mSecondQuote != null) {
+            secondQuoteContent.setText(mSecondQuote.getContent());
+            secondQuoteCategory.setText(DatabaseUtils.getCategoryName(mSecondQuote.getCategoryID(), getActivity().getApplicationContext()));
         }
-        if (favoriteQuote != null) {
-            favoriteQuoteContent.setText(favoriteQuote.getContent());
-        }else{
+        if (mFavoriteQuote != null) {
+            favoriteQuoteContent.setText(mFavoriteQuote.getContent());
+        } else {
             favoriteQuoteContent.setText("No favorites yet!, Save some.");
         }
 
@@ -129,7 +90,7 @@ public class Home extends Fragment implements View.OnClickListener {
         return rootView;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
+
     private void onCardPressed(long quoteID) {
         if (mListener != null) {
             mListener.onQuoteClicked(quoteID);
@@ -157,38 +118,27 @@ public class Home extends Fragment implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.first_category_card:
-                onCardPressed(firstQuote.getId());
+                onCardPressed(mFirstQuote.getId());
                 break;
             case R.id.second_category_card:
-                onCardPressed(secondQuote.getId());
+                onCardPressed(mSecondQuote.getId());
                 break;
             case R.id.favorite_category_card:
-                if (favoriteQuote!=null){
-                    onCardPressed(favoriteQuote.getId());
+                if (mFavoriteQuote != null) {
+                    onCardPressed(mFavoriteQuote.getId());
                 }
                 break;
             case R.id.first_fav:
-                DatabaseUtils.addToFavorites(firstQuote.getId(), getActivity().getApplicationContext());
+                DatabaseUtils.addToFavorites(mFirstQuote.getId(), getActivity().getApplicationContext());
                 break;
             case R.id.second_fav:
-                DatabaseUtils.addToFavorites(secondQuote.getId(), getActivity().getApplicationContext());
+                DatabaseUtils.addToFavorites(mSecondQuote.getId(), getActivity().getApplicationContext());
                 break;
-
 
         }
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-
+    //      This interface must be implemented by activities that contain this fragment
     public interface OnQuoteClickedListener {
         // TODO: Update argument type and name
         void onQuoteClicked(long quoteID);
