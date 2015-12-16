77af2eccd6bda85b5ef050663d17b6c5a2923cc6
AmrElmasry <amrelmasry.me@gmail.com>
2015-12-16 04:36:42 +0200

    Single Quote fragment added

--- app/src/main/java/com/android/people/quotesandroidapp/ui/SingleQuoteActivity.java
+++ app/src/main/java/com/android/people/quotesandroidapp/ui/SingleQuoteActivity.java
@@ -2,13 +2,10 @@ package com.android.people.quotesandroidapp.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 
 import com.android.people.quotesandroidapp.R;
 
@@ -21,14 +18,6 @@ public class SingleQuoteActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fav_single_quote);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
